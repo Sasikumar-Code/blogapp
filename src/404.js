f780f744f3d6c2a0f02aefc4b51bb41d56f509bf
Sasikumar-Code <sasiias2024@gmail.com>
2023-11-20 21:12:44 +0530

    add

---
+++ src/404.js
@@ -0,0 +1,13 @@
+/** @format */
+
+import React from 'react';
+
+const notFound = () => {
+  return (
+    <div>
+      <h1> 404 - Page not found</h1>
+    </div>
+  );
+};
+
+export default notFound;
